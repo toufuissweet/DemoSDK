36874f2394a1e22592ab34fd1bb885c9bbbb0529
toufuissweet <759155865@qq.com>
2021-01-21 11:27:48 +0800

    Initial commit

---
+++ Example/demo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by toufuissweet on 01/21/2021.
+//  Copyright (c) 2021 toufuissweet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
